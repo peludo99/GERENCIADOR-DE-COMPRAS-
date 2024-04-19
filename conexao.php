05a1ad33143e2a2fbf59d9a2b036889abc94b830
peludo <xdazcauanabraao@gmail.com>
2024-04-19 00:22:20 -0300

    Adicão de arquivos Basicos

--- conexao.php
+++ conexao.php
@@ -10,13 +10,13 @@ class Pessoa
 
             $this->pdo = new PDO("mysql:dbname=" . $dbname . ";host=" . $host, $user, $pass);
         } catch (PDOException $e) {
-            Mensagem("Falha ao conectar-se ao banco de dados por favor entre em contato com o administrador!","danger").$e;
-            echo"<a href='http://wa.me/5581983347589'>entre em contato aqui </a> "; 
-            
+            Mensagem("Falha ao conectar-se ao banco de dados por favor entre em contato com o administrador!", "danger") . $e;
+            echo "<a href='http://wa.me/5581983347589'>entre em contato aqui </a> ";
+
 
             exit();
         } catch (Exception $e) {
-            Mensagem("ocorreu um erro","danger").$e;
+            Mensagem("ocorreu um erro", "danger") . $e;
             exit();
         }
     }
@@ -26,11 +26,34 @@ class Pessoa
     {
         $res = array();
         $cmd = $this->pdo->prepare("SELECT * FROM faturas WHERE nome_dono = :n ORDER BY Idfatura");
-        $cmd->bindValue(":n",$nome_dono);
+        $cmd->bindValue(":n", $nome_dono);
+        $cmd->execute();
+        $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
+
+
+        return $res;
+    }
+
+    function DeletarFaturas($nome_dono)
+    {
+        $cmd = $this->pdo->prepare("DELETE FROM faturas
+        WHERE prestacoes = 0 and nome_dono = :n;
+        ");
+        $cmd->bindValue(":n", $nome_dono);
+        $cmd->execute();
+    }
+
+    public function buscarValor($nome_dono)
+    {
+        $res =  "";
+        $cmd = $this->pdo->prepare("SELECT SUM(mensalidade) FROM `faturas` WHERE nome_dono = :n");
+        $cmd->bindValue(":n", $nome_dono);
         $cmd->execute();
         $res = $cmd->fetchAll(PDO::FETCH_ASSOC);
         return $res;
     }
+
+
 
     public function AdicionarFatura($nome, $nome_fatura, $mensalidade, $prestacao)
     // tirei a data
@@ -45,48 +68,40 @@ class Pessoa
         $cmd->execute();
 
         if ($cmd->rowCount() > 0) {
-            Mensagem("OPS!, Essa Fatura ja foi cadastrada!","danger");
+            Mensagem("OPS!, Essa Fatura ja foi cadastrada!", "danger");
             exit();
             return false;
-         
-        }
-
-      else{
+        } else {
             $cmd = $this->pdo->prepare("INSERT INTO  faturas (nome_fatura, mensalidade, prestacoes, nome_dono) VALUES(:f, :m, :p,:d)");
-             
 
-             $cmd->bindValue(":f",$nome_fatura);
-             $cmd->bindValue(":m",$mensalidade);
-             $cmd->bindValue(":p", $prestacao);
-             $cmd->bindValue(":d", $nome);
+
+            $cmd->bindValue(":f", $nome_fatura);
+            $cmd->bindValue(":m", $mensalidade);
+            $cmd->bindValue(":p", $prestacao);
+            $cmd->bindValue(":d", $nome);
             //  $cmd->bindValue(":d", $data);
-            Mensagem("Essa Fatura foi cadastrada!","success");
+            Mensagem("Essa Fatura foi cadastrada!", "success");
 
-             $cmd->execute();
-             
+            $cmd->execute();
 
-             return true;
 
+            return true;
         }
     }
 
-
-
+    function SubtrairPestacoes($nome_dono)
+    {
+        $cmd = $this->pdo->prepare("UPDATE faturas SET prestacoes = prestacoes -1 WHERE nome_dono = :n;");
+        $cmd->bindValue(":n", $nome_dono);
+        $cmd->execute();
+    }
 }
 
-    function Mensagem($mensagem,$tipo)
-      {
+function Mensagem($mensagem, $tipo)
+{
 
-        echo"<div class='alert alert-$tipo' role='alert'>
+    echo "<div class='alert alert-$tipo' role='alert'>
              $mensagem
              </div>
-             <br>
-             <form action='tela_inicial.php' method='post'>
-                <input type='submit' value='voltar' class='btn btn-danger'>
-             </form>";
-             
-    
-
-      }
-
-?>
+             <br>";
+}
--- confirmar_pagamento.php
+++ confirmar_pagamento.php
@@ -1,36 +1,72 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-<head>
-     <!-- Required meta tags -->
-     <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <!-- Bootstrap CSS -->
-        <link rel="stylesheet" href="css/bootstrap.min.css">
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="refresh" content="5; URL=./tela_inicial.php">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
     <title>Confirmar</title>
 </head>
+
 <body class="meio">
-        <form class="d-flex" action="consultar_fatura.php" method="post">
-            <input class="form-control me-2" type="number" placeholder="Digite o cpf do comprador" aria-label="Search" name="buscar" required>
-                    
-            <button class="btn btn-outline-success" type="submit">Consultar</button>
-        </form>
+    <?php
+    require_once 'conexao.php';
+
+    $p = new Pessoa('Rose', '127.0.0.1', 'root', 'root');
+
+    if (isset($_GET["id"])) {
+        $pesquisar = $_GET["id"];
+
+        $p->SubtrairPestacoes($pesquisar);
+    }
 
 
-    
 
 
- <!-- Optional JavaScript; choose one of the two! -->
 
-        <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
-        <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
 
-        <!-- Option 2: Separate Popper and Bootstrap JS -->
-        <!--
+    ?>
+
+
+
+    <table>
+        <tr>
+            <td>
+                <h1>Pagamento foi Confirmado</h1>
+
+            </td>
+        </tr>
+        <tr>
+            <td>
+                <h3>Seus valores foram atualizados:</h3>
+            </td>
+        </tr>
+
+        <tr>
+            <td>
+                <h5> Aguarde o Sistema redireciona-lo(la)...</h5>
+            </td>
+        </tr>
+    </table>
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js" integrity="sha384-+sLIOodYLS7CIrQpBjl+C7nPvqq+FbNUBDunl/OZv93DB7Ln/533i8e/mZXLi/P+" crossorigin="anonymous"></script>
         -->
 </body>
+
 </html>
